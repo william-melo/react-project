7a10c82e365308862c6223ca7f1b538781ec1808
William Melo <williamandresmeloburgos@gmail.com>
2024-04-22 11:47:09 -0500

    confirmacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,13 @@
-import "./App.css";
-//import results from "./mocks/results.json";
-import { Pictures } from "./components/Pictures";
-import { Footer } from "./components/Footer";
-import { FavoriteProvider } from "./contexts/favorite";
-import { usePictures } from "./hooks/usePictures.js";
-import { useSearch } from "./hooks/useSearch.js";
-import debounce from "just-debounce-it";
-import { useCallback } from "react";
+import Home from "./Home";
+import "./App.css"
+
 
 function App() {
-  //const pictures = results.results
-
-  const { search } = useSearch();
-  const { getPictures, pictures } = usePictures({ search });
-
-  const debouncedPictures = useCallback(
-    debounce((search) => {
-      getPictures({ search });
-    }, 700)
-  );
-
-  // Header
-  function Header() {
-    const { search, setSearch, error } = useSearch();
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      getPictures({ search });
-    };
-
-    const handleChange = (event) => {
-      const newSearch = event.target.value;
-      setSearch(newSearch);
-      debouncedPictures(newSearch);
-    };
-
+  
     return (
-      <header>
-        <h1>React Pictures</h1>
-        <form onSubmit={handleSubmit}>
-          <div className="search-container">
-            <input
-              type="text"
-              placeholder="Sun, dogs, sunset, beach..."
-              value={search}
-              name="query"
-              onChange={handleChange}
-            />
-            <button>Search</button>
-          </div>
-        </form>
-        {error && <p className="error-message">{error}</p>}
-      </header>
+        <Home />
     );
   }
-
-  return (
-    <FavoriteProvider>
-      <Header />
-      <Pictures pictures={pictures} />
-      <Footer />
-    </FavoriteProvider>
-  );
-}
-
-export default App;
+  
+  export default App;
+  
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
-
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { FavoriteProvider } from "./contexts/favorite.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <FavoriteProvider>
+    <App />
+  </FavoriteProvider>
+);
