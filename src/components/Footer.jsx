87599cd2e7deb159e75e5efe2a06dda99df49873
William Melo <williamandresmeloburgos@gmail.com>
2024-04-22 19:45:58 -0500

    adding link to my github in the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export function Footer () {
 
   return (
     <footer className='footer'>
-      <h4>Prueba técnica de React ⚛️ － <span>@william_melo</span></h4>
+      <h4>Prueba técnica de React ⚛️ － <span><a href="https://github.com/william-melo">@william_melo</a></span></h4>
       <h5>Gallery e Imagenes Favoritas</h5>
     </footer>
   )
